aa1f9b9df595ef4fd9e0896d8ba62cec96ea6093
Piyush Rajesh Deshkar <shacx8@gmail.com>
2026-03-08 15:50:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const SUPABASE_URL = "https://nnqoiiewxhcpujxnecfn.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ucW9paWV3eGhjcHVqeG5lY2ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NjMxMTIsImV4cCI6MjA4ODUzOTExMn0.tEXH_wLebXydl6-GG3NYma9G6e8TJpBSfbrevIjJDh4
+  "
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
 const correctPin = "4632"
 
 function unlock(){
